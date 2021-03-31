779ca4f47c061f313504a89c53d6babf16829091
jeffinmeister <jeffinpoulose2@gmail.com>
2021-03-31 08:02:55 +0530

    second update on env

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use('/auth',require('./routes/auth'))
 app.use('/stories',require('./routes/stories'))
 
 //set port
-//const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.listen( process.env.PORT || 3000, console.log(`Server running  in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
